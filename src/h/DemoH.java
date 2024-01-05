f6d75f786a5a2c04f0fc8c6ede296aaa29cd087d
kamisury23 <155509146+kamisury23@users.noreply.github.com>
2024-01-05 17:57:54 -0500

    Update DemoH.java

--- src/h/DemoH.java
+++ src/h/DemoH.java
@@ -5,6 +5,9 @@ public class DemoH {
 	public static void main(String[] args) {
 		
 		System.out.println("Snow is coming...");
+
+		//coming from github. 
+		System.out.println("Studying java all weekend");
 		
 	}
 
